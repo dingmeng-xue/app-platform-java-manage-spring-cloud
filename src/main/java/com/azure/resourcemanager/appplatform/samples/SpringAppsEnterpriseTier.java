7adde7b94ededd81d6ceb3fe9050fd20bf8d03ea
Dingmeng Xue <dixue@microsoft.com>
2023-06-05 20:09:33 +0800

    Add app deployment

--- src/main/java/com/azure/resourcemanager/appplatform/samples/SpringAppsEnterpriseTier.java
+++ src/main/java/com/azure/resourcemanager/appplatform/samples/SpringAppsEnterpriseTier.java
@@ -1,6 +1,9 @@
 package com.azure.resourcemanager.appplatform.samples;
 
+import java.io.File;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.policy.HttpLogDetailLevel;
@@ -12,21 +15,11 @@ import com.azure.core.management.profile.AzureProfile;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.resourcemanager.AzureResourceManager;
 import com.azure.resourcemanager.appplatform.AppPlatformManager;
-import com.azure.resourcemanager.appplatform.fluent.AppPlatformManagementClient;
-import com.azure.resourcemanager.appplatform.fluent.models.AppResourceInner;
 import com.azure.resourcemanager.appplatform.fluent.models.BuildServiceAgentPoolResourceInner;
 import com.azure.resourcemanager.appplatform.fluent.models.BuildServiceInner;
 import com.azure.resourcemanager.appplatform.fluent.models.BuilderResourceInner;
-import com.azure.resourcemanager.appplatform.fluent.models.DeploymentResourceInner;
-import com.azure.resourcemanager.appplatform.models.AppResourceProperties;
-import com.azure.resourcemanager.appplatform.models.BuildResultUserSourceInfo;
-import com.azure.resourcemanager.appplatform.models.DeploymentResourceProperties;
-import com.azure.resourcemanager.appplatform.models.DeploymentSettings;
-import com.azure.resourcemanager.appplatform.models.ResourceRequests;
-import com.azure.resourcemanager.appplatform.models.Sku;
-import com.azure.resourcemanager.appplatform.models.SkuName;
+import com.azure.resourcemanager.appplatform.models.SpringApp;
 import com.azure.resourcemanager.appplatform.models.SpringService;
-import com.azure.resourcemanager.appplatform.models.TemporaryDisk;
 import com.azure.resourcemanager.resources.models.ResourceGroup;
 import com.azure.resourcemanager.samples.Utils;
 
@@ -38,7 +31,7 @@ public class SpringAppsEnterpriseTier {
 	private static final Region DEFAULT_REGION = Region.US_EAST;
 
 	private static AzureResourceManager azureResourceManager;
-	private static AppPlatformManagementClient svcClient;
+	private static AppPlatformManager asaSvcManager;
 
 	/**
 	 * Main entry point.
@@ -49,13 +42,18 @@ public class SpringAppsEnterpriseTier {
 		try {
 			// =============================================================
 			// Authenticate
-
 			final AzureProfile profile = new AzureProfile(TENANT_ID, SUBSCRIPTION_ID, AzureEnvironment.AZURE);
 			final TokenCredential credential = new DefaultAzureCredentialBuilder()
 					.authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint()).build();
 
+			
+			// Create ARM client for resource group management
 			azureResourceManager = AzureResourceManager.configure().withLogLevel(HttpLogDetailLevel.BASIC)
 					.authenticate(credential, profile).withSubscription(SUBSCRIPTION_ID);
+
+			// Create ASA service client for Azure Spring Apps resource management
+			asaSvcManager = AppPlatformManager.configure().withLogLevel(HttpLogDetailLevel.BASIC)
+					.authenticate(credential, profile);
 
 			// Print selected subscription
 			System.out.printf("Selected subscription %s%n", azureResourceManager.subscriptionId());
@@ -63,18 +61,14 @@ public class SpringAppsEnterpriseTier {
 			String rgName = Utils.randomResourceName(azureResourceManager, "rg-", 24);
 			String serviceName = Utils.randomResourceName(azureResourceManager, "demo-svc-", 24);
 
-
 			// Get or create resource group
 			ResourceGroup rg = getOrCreateResourceGroup(rgName);
-			// Get or create spring apps service
-			SpringService service = getOrCreateSpringAppsService(rg, serviceName);
+			
+			// Get or create Azure Spring Apps service
+			SpringService service = getOrCreateASAService(rg, serviceName);
 
-			AppPlatformManager manager = AppPlatformManager.configure().withLogLevel(HttpLogDetailLevel.BASIC)
-					.authenticate(credential, profile);
-			svcClient = manager.serviceClient();
-
-			PagedIterable<BuildServiceInner> buildServices = svcClient.getBuildServices().listBuildServices(rg.name(),
-					service.name());
+			PagedIterable<BuildServiceInner> buildServices = asaSvcManager.serviceClient().getBuildServices()
+					.listBuildServices(rg.name(), service.name());
 			BuildServiceInner buildService = buildServices.iterator().next();
 			if (buildService != null) {
 				System.out.printf("Get default build service %s%n", buildService.name());
@@ -82,8 +76,8 @@ public class SpringAppsEnterpriseTier {
 				throw new Exception("Cannot find default build service");
 			}
 
-			PagedIterable<BuilderResourceInner> builders = svcClient.getBuildServiceBuilders().list(rg.name(),
-					service.name(), "default");
+			PagedIterable<BuilderResourceInner> builders = asaSvcManager.serviceClient().getBuildServiceBuilders()
+					.list(rg.name(), service.name(), "default");
 			BuilderResourceInner builder = builders.iterator().next();
 			if (builder != null) {
 				System.out.printf("Get default builder %s%n", builder.name());
@@ -91,8 +85,8 @@ public class SpringAppsEnterpriseTier {
 				throw new Exception("Cannot find default builder");
 			}
 
-			PagedIterable<BuildServiceAgentPoolResourceInner> agentPools = svcClient.getBuildServiceAgentPools()
-					.list(rg.name(), service.name(), "default");
+			PagedIterable<BuildServiceAgentPoolResourceInner> agentPools = asaSvcManager.serviceClient()
+					.getBuildServiceAgentPools().list(rg.name(), service.name(), "default");
 			BuildServiceAgentPoolResourceInner agentPool = agentPools.iterator().next();
 			if (agentPool != null) {
 				System.out.printf("Get default agent pool %s%n", agentPool.name());
@@ -100,7 +94,22 @@ public class SpringAppsEnterpriseTier {
 				throw new Exception("Cannot find default agent pool");
 			}
 
-			deployApplication(service, "hello-world", null);
+			// Create application without deployment
+			SpringApp helloWorldApp = getOrCreateASAApp(service, "hello-world");
+
+			// load JAR file from project
+			File appFile = Files.createTempFile("asatemp", ".jar").toFile();
+			try (InputStream is = SpringAppsEnterpriseTier.class
+					.getResourceAsStream("/hello-world-0.0.1-SNAPSHOT.jar")) {
+				Files.copy(is, appFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			}
+			
+			// deploy JAR file to deployment
+			helloWorldApp = deployApplication(helloWorldApp, appFile);
+
+			// Assign an endpoint to application
+			helloWorldApp = service.apps().define(helloWorldApp.name()).withDefaultActiveDeployment().withDefaultPublicEndpoint().create();
+			System.out.printf("Please visit app via %s%n", helloWorldApp.url());
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -116,9 +125,9 @@ public class SpringAppsEnterpriseTier {
 	 */
 	private static ResourceGroup getOrCreateResourceGroup(String name) {
 		ResourceGroup rg = null;
-		try {
+		if (azureResourceManager.resourceGroups().contain(name)) {
 			rg = azureResourceManager.resourceGroups().getByName(name);
-		} catch (ManagementException e) {
+		} else {
 			System.out.printf("Creating resource group %s%n", name);
 
 			rg = azureResourceManager.resourceGroups().define(name).withRegion(DEFAULT_REGION).create();
@@ -135,7 +144,7 @@ public class SpringAppsEnterpriseTier {
 	 * @param name
 	 * @return
 	 */
-	private static SpringService getOrCreateSpringAppsService(ResourceGroup rg, String name) {
+	private static SpringService getOrCreateASAService(ResourceGroup rg, String name) {
 		SpringService service = null;
 		try {
 			service = azureResourceManager.springServices().getByResourceGroup(rg.name(), name);
@@ -143,73 +152,32 @@ public class SpringAppsEnterpriseTier {
 		} catch (ManagementException e) {
 			System.out.printf("Creating Azure Spring Apps service %s in resource group %s ...%n", name, rg.name());
 
-			service = azureResourceManager.springServices().define(name).withRegion(DEFAULT_REGION)
-					.withExistingResourceGroup(rg.name()).withSku(SkuName.E0).create();
+			service = asaSvcManager.springServices().define(name).withRegion(DEFAULT_REGION)
+					.withExistingResourceGroup(rg.name()).withEnterpriseTierSku().create();
 
 			System.out.printf("Created Azure Spring Apps service %s%n", service.name());
 		}
 		return service;
 	}
 
-	private static void deployApplication(SpringService service, String appName, InputStream appIs) {
-		AppResourceInner app = getOrCreateASAApp(service, appName);
-		DeploymentResourceInner deployment = getOrCreateDefaultDeployment(service, app);
-
-		return;
-	}
-
-	private static AppResourceInner getOrCreateASAApp(SpringService service, String appName) {
-		AppResourceInner app = null;
-
+	private static SpringApp getOrCreateASAApp(SpringService service, String appName) {
+		SpringApp app = null;
 		try {
-			app = svcClient.getApps().get(service.resourceGroupName(), service.name(), appName);
-			System.out.printf("Got app %s in service %s%n", app.name(), service.name());
+			app = service.apps().getByName(appName);
+			System.out.printf("Got application %s in service %s%n", appName, service.name());
 		} catch (ManagementException e) {
-			System.out.printf("Creating app %s in service %s ...%n", appName, service.name());
+			System.out.printf("Creating app %s in Azure Spring Apps service %s ...%n", appName, service.name());
 
-			AppResourceProperties appProps = new AppResourceProperties();
+			app = service.apps().define(appName).withDefaultActiveDeployment().create();
 
-			appProps.withPublicProperty(false).withHttpsOnly(false)
-					.withTemporaryDisk(new TemporaryDisk().withSizeInGB(5).withMountPath("/temp"))
-					.withEnableEndToEndTls(false);
-			app = svcClient.getApps().createOrUpdate(service.resourceGroupName(), service.name(), appName,
-					new AppResourceInner().withProperties(appProps));
-
-			System.out.printf("Created app %s in service %s%n", app.name(), service.name());
+			System.out.printf("Created app in Azure Spring Apps service %s%n", appName, service.name());
 		}
-
 		return app;
 	}
 
-	private static DeploymentResourceInner getOrCreateDefaultDeployment(SpringService service, AppResourceInner app) {
-		DeploymentResourceInner deployment = null;
-
-		try {
-			deployment = svcClient.getDeployments().get(service.resourceGroupName(), service.name(), app.name(),
-					"default");
-			System.out.printf("Got default deployment of app %s%n", app.name());
-		} catch (ManagementException e) {
-			System.out.printf("Creating default deployment for app %s ...%n", app.name());
-
-			DeploymentSettings settings = new DeploymentSettings();
-			settings.withResourceRequests(new ResourceRequests().withCpu("1").withMemory("1Gi"));
-
-			DeploymentResourceProperties deployProps = new DeploymentResourceProperties();
-			deployProps.withActive(true);
-			deployProps.withDeploymentSettings(settings);
-			deployProps.withSource(new BuildResultUserSourceInfo().withBuildResultId("<default>"));
-
-			Sku skuInfo = new Sku();
-			skuInfo.withTier("Enterprise");
-			skuInfo.withName("E0");
-			skuInfo.withCapacity(1);
-
-			deployment = svcClient.getDeployments().createOrUpdate(service.resourceGroupName(), service.name(),
-					app.name(), "default", new DeploymentResourceInner().withProperties(deployProps).withSku(skuInfo));
-
-			System.out.printf("Created default deployment for app %s%n", app.name());
-		}
-
-		return deployment;
+	private static SpringApp deployApplication(SpringApp app, File appFile) {
+		app.deployments().define("default").withJarFile(appFile).withInstance(2).withCpu(1).withMemory(1)
+				.withJvmOptions("-DskipTests=true").withActivation().create();
+		return app;
 	}
 }
